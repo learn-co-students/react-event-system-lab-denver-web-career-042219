1c8e2381f6b45be0d6d11639e5f4e5de8b782ad8
drbarq <J.Tustin@gmail.com>
2019-06-26 20:00:09 -0600

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,32 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+
+    handleFocus = () => {
+        console.log("Good!")
+    }
+
+    handleBlur = () => {
+        console.log("Hey! Eyes on me!")
+    }
+
+    render() {
+        return(
+            <React.Fragment>
+                <button 
+                    onFocus = {this.handleFocus}
+                    onBlur = {this.handleBlur} 
+                    />
+                {/* <input 
+                    type="button"
+                    onFocus = {this.handleFocus}
+                    onBlur = {this.handleBlur} 
+                /> */}
+            </React.Fragment>
+        )
+    }
+
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,24 @@
 // Code Keypad Component Here
+
+import React from 'react'
+
+class Keypad extends React.Component {
+    
+    handleInputPassword = () => {
+        console.log('Entering password...')
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                {/* <h1>Hello</h1> */}
+                <input 
+                    type="password" 
+                    onKeyUp = {this.handleInputPassword}
+                    />
+            </React.Fragment>
+        )
+    }
+}
+
+export default Keypad;
